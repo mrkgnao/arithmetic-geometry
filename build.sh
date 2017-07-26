30cc7214e6c9de4cf7e4aa219cd27680221cdfbd
Soham Chowdhury <chow.soham@gmail.com>
2017-07-26 20:37:58 +0530

    Close to finishing power series section

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# mkdir -p _shake
+mkdir -p _shake
 
-# stack --resolver lts-8.22 \
-#   --install-ghc ghc \
-#   --package shake \
-#   -- \
-#   --make Build.hs \
-#   -O0 \
-#   -threaded \
-#   -rtsopts -with-rtsopts="-I0 -qg -qb" \
-#   -outputdir=_shake \
-#   -o _shake/build
+stack --resolver lts-8.23 \
+  --install-ghc ghc \
+  --package shake \
+  -- \
+  --make Build.hs \
+  -O0 \
+  -threaded \
+  -rtsopts -with-rtsopts="-I0 -qg -qb" \
+  -outputdir=_shake \
+  -o _shake/build
 
 _shake/build "$@"
